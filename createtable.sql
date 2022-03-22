ab699b7fcafbdb00879864adc1e16176461ea44e
mich.li <mich.li@cgi.com>
2022-03-22 07:49:52 -0600

    update

---
+++ createtable.sql
@@ -0,0 +1,2 @@
+CREATE TABLE sushi ( name varchar(20), price float);
+CREATE TABLE ramen ( name varchar(20), price float);
---
+++ insertvalue.sql
@@ -0,0 +1,3 @@
+INSERT INTO sushi (name, price) VALUES ('Tuna',5);
+INSERT INTO sushi (name, price) VALUES ('Uni',10);
+INSERT INTO sushi (name, price) VALUES ('Unagi',3);
